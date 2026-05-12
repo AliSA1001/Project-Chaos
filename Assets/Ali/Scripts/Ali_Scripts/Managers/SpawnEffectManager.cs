1ca14d9e0979da9c334fae3b1c084b024fd411be
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-05-13 02:42:36 +0300

    save

--- Assets/Ali/Scripts/Ali_Scripts/Managers/SpawnEffectManager.cs
+++ Assets/Ali/Scripts/Ali_Scripts/Managers/SpawnEffectManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SpawnEffectManger : MonoBehaviour
+public class SpawnEffectManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
