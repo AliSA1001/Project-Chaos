262c8d69f2c4b37a01bec2824a8828bf6c1d3684
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-05-13 12:51:14 +0300

    blood

--- Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
+++ Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
@@ -6,12 +6,8 @@ using UnityEngine.AI;
 public class NormalEnemy : MonoBehaviour, IDamgeable
 {
     [SerializeField] private float enemyHp;
-    [SerializeField] private Transform playerPOS;
+   [SerializeField] private Transform playerPOS;
 
-
-    [Header("Effect")]
-    [SerializeField] private AudioSource hitmarkerSound;
-    [SerializeField] private GameObject hitmarker;
 
     [Header("Score Amount")]
     [SerializeField] private int hitAmount = 10;
@@ -21,6 +17,7 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
     private Stats staInstance;
     private FpController playerInstance;
     private ScoreManager scoreManager;
+    private SpawnEffectManager spawnEffectManager;
 
     private void Awake()
     {
@@ -31,6 +28,9 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
         staInstance = Stats.instance;
         playerInstance = FpController.instance;
         scoreManager = ScoreManager.instance;
+        spawnEffectManager = SpawnEffectManager.Instance;
+
+        playerPOS = playerInstance.gameObject.transform;
     }
 
 
@@ -44,13 +44,12 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
 
     public void TakeDamage(float amount)
     {
-        hitmarkerSound.Play();
-        hitmarker.SetActive(true);
-        Invoke("RemoveHitmarker", 0.5f);
+        spawnEffectManager.AddHitMarkerEffect();
         enemyHp -= amount;
         if (enemyHp <= 0)
         {
             scoreManager.AddKillScore(killAmount);
+            spawnEffectManager.SpawnBloodBlastEffect(gameObject.transform);
             Destroy(gameObject);
         }
         else
@@ -58,13 +57,9 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
             scoreManager.AddHitScore(hitAmount);
         }
 
-        
     }
 
-    private void RemoveHitmarker()
-    {
-        hitmarker.SetActive(false);
-    }
+  
    
     
     private void OnTriggerStay(Collider other)
--- Assets/Ali/Scripts/Ali_Scripts/Managers/SpawnEffectManager.cs
+++ Assets/Ali/Scripts/Ali_Scripts/Managers/SpawnEffectManager.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class SpawnEffectManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SpawnEffectManager Instance;
+
+
+    [SerializeField] private GameObject bloodBlast;
+    [SerializeField] private GameObject hitMarker;
+    [SerializeField] private AudioSource hitMarkerSound;
+   // [SerializeField] private AudioSource enemyDeathSound;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnBloodBlastEffect(Transform bloodSpawn)
     {
-        
+        Instantiate(bloodBlast , bloodSpawn.position, bloodSpawn.rotation);
+     //   enemyDeathSound.Play();
+    }
+
+    public void AddHitMarkerEffect()
+    {
+        hitMarkerSound.Play();
+        hitMarker.SetActive(true);
+        Invoke("RemoveHitmarker", 0.5f);
+    }
+
+    private void RemoveHitmarker()
+    {
+        hitMarker.SetActive(false);
     }
 }
