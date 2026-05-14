f2804d18dd5ce84750e9cdac7e1a4b3594958d94
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-05-14 20:33:36 +0300

    Reapply "save plze"
    This reverts commit 203e10cb450f75dd0fed7fd6a861bb3e9b3855d5.

--- Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
+++ Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
@@ -1,44 +1,29 @@
-using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NormalEnemy : MonoBehaviour, IDamgeable
+public class NormalEnemy : MonoBehaviour , IDamgeable
 {
     [SerializeField] private float enemyHp;
-   [SerializeField] private Transform playerPOS;
-
-
-    [Header("Score Amount")]
-    [SerializeField] private int hitAmount = 10;
-    [SerializeField] private int killAmount = 100;
+    [SerializeField] private Transform playerPOS;
 
     private NavMeshAgent agent;
     private Stats staInstance;
     private FpController playerInstance;
-    private ScoreManager scoreManager;
-    private SpawnEffectManager spawnEffectManager;
-    private Animator animator;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
     }
     private void Start()
     {
         staInstance = Stats.instance;
         playerInstance = FpController.instance;
-        scoreManager = ScoreManager.instance;
-        spawnEffectManager = SpawnEffectManager.Instance;
-
-        playerPOS = playerInstance.gameObject.transform;
     }
 
 
     private void Update()
     {
-        animator.SetBool("Moving" , true);
         float distance = Vector3. Distance(transform.position, transform.forward);
         //  if(!agent.pathPending && distance   )
         agent.SetDestination(playerPOS.position);
@@ -47,24 +32,13 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
 
     public void TakeDamage(float amount)
     {
-        spawnEffectManager.AddHitMarkerEffect();
         enemyHp -= amount;
         if (enemyHp <= 0)
         {
-            scoreManager.AddKillScore(killAmount);
-            spawnEffectManager.SpawnBloodBlastEffect(gameObject.transform);
             Destroy(gameObject);
         }
-        else
-        {
-            scoreManager.AddHitScore(hitAmount);
-        }
-
     }
 
-  
-   
-    
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject == playerInstance.gameObject)
