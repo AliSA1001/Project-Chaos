1bd040eb5e0e409671d8296ac583329ba3eb77d4
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-05-19 21:50:24 +0300

    save

--- Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
+++ Assets/Ali/Scripts/Ali_Scripts/Enemies/NormalEnemy.cs
@@ -18,10 +18,12 @@ public class NormalEnemy : MonoBehaviour, IDamgeable
     private FpController playerInstance;
     private ScoreManager scoreManager;
     private SpawnEffectManager spawnEffectManager;
+    private Animator animator;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
     private void Start()
     {
